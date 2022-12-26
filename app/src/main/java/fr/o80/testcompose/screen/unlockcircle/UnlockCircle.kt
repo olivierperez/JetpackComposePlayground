0e4b5c82f1fef1a04b8e7ae94be359b4d024db59
Olivier PEREZ <olivier@olivierperez.fr>
2022-12-26 17:37:15 +0100

    Increase size of close button

--- app/src/main/java/fr/o80/testcompose/screen/unlockcircle/UnlockCircle.kt
+++ app/src/main/java/fr/o80/testcompose/screen/unlockcircle/UnlockCircle.kt
@@ -3,6 +3,7 @@ package fr.o80.testcompose.screen.unlockcircle
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.o80.testcompose.ui.theme.TestComposeCanvasTheme
 import kotlin.random.Random
 
@@ -43,7 +45,7 @@ fun UnlockCircle(
                 onFullyUnlocked = { finished = true }
             )
         }
-        IconButton(onClick = onClose) {
+        IconButton(onClick = onClose, modifier = Modifier.size(64.dp)) {
             Icon(Icons.Default.Close, contentDescription = "Close")
         }
     }
