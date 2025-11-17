5fd9bc2cea669cbb07a77f2a9c4bd4a0831798bd
Olivier PEREZ <olivier@olivierperez.fr>
2025-11-17 13:00:43 +0100

    apply shader as a render effect

--- app/src/main/java/fr/o80/testcompose/screen/shader/ShaderScreen.kt
+++ app/src/main/java/fr/o80/testcompose/screen/shader/ShaderScreen.kt
@@ -2,6 +2,7 @@ package fr.o80.testcompose.screen.shader
 
 import android.animation.ValueAnimator
 import android.graphics.Color
+import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
 import android.os.Build
 import android.view.animation.LinearInterpolator
@@ -21,10 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.ShaderBrush
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -54,50 +53,44 @@ fun ShaderScreen(
 
 private const val COLOR_SHADER_SRC = """
     layout(color) uniform half4 iColor;
-    uniform half2 iResolution;
-    uniform float iTime;
+    uniform float strength;
     
     uniform shader inputShader;
     
     half4 main(float2 fragCoord) {
         float4 pixel = inputShader.eval(fragCoord);
-        return mix(pixel, iColor, abs(sin(iTime)));
+        return mix(pixel, iColor, pixel.a * strength);
     }
 """
 
 @Composable
 private fun Modifier.glitch(): Modifier {
     return if (Build.VERSION.SDK_INT >= 33) {
-        var time by remember { mutableFloatStateOf(0f) }
+        var strength by remember { mutableFloatStateOf(0f) }
         val shader = RuntimeShader(COLOR_SHADER_SRC)
 
         // Animator
         LaunchedEffect(Unit) {
-            val duration = 5000f
-            val animator = ValueAnimator.ofFloat(0f, 2f * Math.PI.toFloat())
+            val duration = 1000f
+            val animator = ValueAnimator.ofFloat(0f, 1f)
             animator.duration = duration.toLong()
             animator.repeatMode = ValueAnimator.REVERSE
             animator.repeatCount = ValueAnimator.INFINITE
             animator.interpolator = LinearInterpolator()
             animator.addUpdateListener { animation ->
-                time = animation.animatedValue as Float
+                strength = animation.animatedValue as Float
             }
             animator.start()
         }
 
-        this.drawWithCache {
-            shader.setFloatUniform("iTime", time)
-            shader.setColorUniform("iColor", Color.GREEN)
-            shader.setFloatUniform("iResolution", size.width, size.height)
-
-            onDrawWithContent {
-                drawContent()
-                drawRect(
-                    brush = ShaderBrush(shader),
-                    size = size,
-                    blendMode = BlendMode.Overlay
-                )
-            }
+        this then graphicsLayer {
+            renderEffect = RenderEffect
+                .createRuntimeShaderEffect(shader, "inputShader")
+                .also {
+                    shader.setFloatUniform("strength", strength)
+                    shader.setColorUniform("iColor", Color.RED)
+                }
+                .asComposeRenderEffect()
         }
     } else {
         this
